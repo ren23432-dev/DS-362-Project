6a55f49000667765e2b13df86ba6f25aff7d7fab
ren23432-dev <ren23432@gmail.com>
2026-08-06 03:29:03 +0300

    Add server-side validation

---
+++ header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Cybersecurity Club</title>
+
+    <link rel="stylesheet" href="../css/styles.css">
+    <script src="../js/validate.js"></script>
+</head>
+
+<body>
+
+<header>
+
+    <div class="container">
+
+        <h1>Cybersecurity Club</h1>
+
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="events.php">Cyber Events</a>
+            <a href="register.php">Register</a>
+            <a href="registrations.php">My Registrations</a>
+            <a href="about.php">About Us</a>
+            <a href="contact.php">Contact Us</a>
+        </nav>
+
+    </div>
+
+</header>
+
+<main class="container">
